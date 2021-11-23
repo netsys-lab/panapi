e6c2163d7e0492b0b81ac1a7b6a3bbc267b5ff3f
Thorben Krüger <thorben.krueger@ovgu.de>
2021-11-23 14:33:39 +0100

    refactored, removed any reference to gopher-luar

--- network/scion/lua_selector.go
+++ network/scion/lua_selector.go
@@ -6,13 +6,20 @@ import (
 	"github.com/netsec-ethz/scion-apps/pkg/pan"
 	"github.com/netsys-lab/panapi/rpc"
 	"github.com/yuin/gopher-lua"
-	"layeh.com/gopher-luar"
 
 	"log"
 	"sync"
 )
 
-func newLuaPath(path *pan.Path) *lua.LTable {
+func new_lua_path_interface(intf pan.PathInterface) *lua.LTable {
+	iface := lua.LTable{}
+	iface.RawSetString("IA", lua.LString(intf.IA.String()))
+	iface.RawSetString("IfID", lua.LNumber(intf.IfID))
+	return &iface
+
+}
+
+func new_lua_path(path *pan.Path) *lua.LTable {
 	t := lua.LTable{}
 	if path != nil {
 		t.RawSetString("Source", lua.LString(path.Source.String()))
@@ -26,10 +33,7 @@ func newLuaPath(path *pan.Path) *lua.LTable {
 
 			ifaces := lua.LTable{}
 			for _, i := range path.Metadata.Interfaces {
-				iface := lua.LTable{}
-				iface.RawSetString("IA", lua.LString(i.IA.String()))
-				iface.RawSetString("IfID", lua.LNumber(i.IfID))
-				ifaces.Append(&iface)
+				ifaces.Append(new_lua_path_interface(i))
 			}
 			meta.RawSetString("Interfaces", &ifaces)
 
@@ -79,7 +83,7 @@ func newLuaPath(path *pan.Path) *lua.LTable {
 	return &t
 }
 
-func newTableFromTableSlice(s []*lua.LTable) *lua.LTable {
+func lua_table_slice_to_table(s []*lua.LTable) *lua.LTable {
 	res := lua.LTable{}
 	for _, t := range s {
 		res.Append(t)
@@ -89,45 +93,38 @@ func newTableFromTableSlice(s []*lua.LTable) *lua.LTable {
 
 type state struct {
 	lpaths map[string]map[string]*lua.LTable
-	ppaths map[string]map[string]*pan.Path
+	ppaths map[*lua.LTable]*pan.Path
 }
 
-func (s state) get_pan_path(raddr pan.UDPAddr, fp string) *pan.Path {
-	if tmp := s.ppaths[raddr.String()]; tmp != nil {
-		return tmp[fp]
+func new_state() state {
+	return state{
+		make(map[string]map[string]*lua.LTable),
+		make(map[*lua.LTable]*pan.Path),
 	}
-	return nil
+}
+
+func (s state) get_pan_path(lpath *lua.LTable) *pan.Path {
+	return s.ppaths[lpath]
 }
 
 func (s state) clear_addr(addr pan.UDPAddr) {
 	raddr := addr.String()
+	for _, lt := range s.lpaths[raddr] {
+		s.ppaths[lt] = nil
+	}
 	s.lpaths[raddr] = map[string]*lua.LTable{}
-	s.ppaths[raddr] = map[string]*pan.Path{}
 }
 
-func (s state) set_pan_paths(addr pan.UDPAddr, paths []*pan.Path) {
+func (s state) set_paths(addr pan.UDPAddr, ppaths []*pan.Path) (lpaths []*lua.LTable) {
 	raddr := addr.String()
-	for _, p := range paths {
-		fp := string(p.Fingerprint)
-		s.ppaths[raddr][fp] = p
-		s.lpaths[raddr][fp] = newLuaPath(p)
-	}
-}
-
-func (s state) set_paths(addr pan.UDPAddr, ppaths []*pan.Path, lpaths []*lua.LTable) {
-	if len(ppaths) != len(lpaths) {
-		log.Panicf("len(ppaths) != len(lpaths) (%d != %d)", len(ppaths), len(lpaths))
-	}
-	raddr := addr.String()
+	lpaths = make([]*lua.LTable, len(ppaths))
 	for i, ppath := range ppaths {
-		fp_p := string(ppath.Fingerprint)
-		fp_l := lpaths[i].RawGetString("Fingerprint").String()
-		if fp_p != fp_l {
-			log.Panicf("ppath.Fingerprint != lpaths.FingerPrint (%s != %s)", fp_p, fp_l)
-		}
-		s.ppaths[raddr][fp_p] = ppath
-		s.lpaths[raddr][fp_l] = lpaths[i]
+		lpath := new_lua_path(ppath)
+		s.lpaths[raddr][string(ppath.Fingerprint)] = lpath
+		s.ppaths[lpath] = ppath
+		lpaths[i] = lpath
 	}
+	return
 }
 
 type LuaSelector struct {
@@ -152,11 +149,8 @@ func NewLuaSelector(script string) (rpc.ServerSelector, error) {
 		L.Push(fn)
 		err = L.PCall(0, lua.MultRet, nil)
 		return &LuaSelector{
-			L: L,
-			state: state{
-				make(map[string]map[string]*lua.LTable),
-				make(map[string]map[string]*pan.Path),
-			},
+			L:     L,
+			state: new_state(),
 		}, err
 	}
 }
@@ -169,23 +163,15 @@ func (s *LuaSelector) SetPaths(raddr pan.UDPAddr, paths []*pan.Path) {
 	//call the "setpaths" function in the Lua script
 	//with two arguments
 	//and don't expect a return value
-	/*s.L.CallByParam(lua.P{
-	Fn:   s.L.GetGlobal("setpaths"),
-	NRet: 0}, luar.New(s.L, raddr), luar.New(s.L, paths))
-	*/
 	s.state.clear_addr(raddr)
-	lpaths := make([]*lua.LTable, len(paths))
-	for i, p := range paths {
-		lpaths[i] = newLuaPath(p)
-	}
-	s.state.set_paths(raddr, paths, lpaths)
+	lpaths := s.state.set_paths(raddr, paths)
 	s.L.CallByParam(
 		lua.P{
 			Fn:   s.L.GetGlobal("setpaths"),
 			NRet: 0,
 		},
 		lua.LString(raddr.String()),
-		newTableFromTableSlice(lpaths),
+		lua_table_slice_to_table(lpaths),
 	)
 }
 
@@ -198,25 +184,11 @@ func (s *LuaSelector) Path(raddr pan.UDPAddr) *pan.Path {
 	//expect 1 return value
 	s.L.CallByParam(lua.P{
 		Fn:   s.L.GetGlobal("selectpath"),
-		NRet: 1}, luar.New(s.L, raddr))
-	//convert top of the stack back to a UserData type
+		NRet: 1}, lua.LString(raddr.String()))
 	lt := s.L.ToTable(-1)
-	//lv := s.L.ToUserData(-1)
 	//pop element from the stack
 	s.L.Pop(1)
-	//try casting the return value back to a *pan.Path
-	/*if p, ok := lv.Value.(*pan.Path); ok {
-		//		log.Printf("lua returned path %v", p)
-		return p
-	} else {
-		//couldn't be casted
-		panic("something went wrong with Lua")
-	}*/
-	tmp := s.state.ppaths[raddr.String()]
-	if tmp == nil {
-		panic("got nothing")
-	}
-	return tmp[lt.RawGetString("Fingerprint").String()]
+	return s.state.get_pan_path(lt)
 }
 
 func (s *LuaSelector) OnPathDown(raddr pan.UDPAddr, fp pan.PathFingerprint, pi pan.PathInterface) {
@@ -226,9 +198,15 @@ func (s *LuaSelector) OnPathDown(raddr pan.UDPAddr, fp pan.PathFingerprint, pi p
 	//call the "onpathdown" function in the Lua script
 	//with two arguments
 	//and don't expect a return value
-	err := s.L.CallByParam(lua.P{
-		Fn:   s.L.GetGlobal("onpathdown"),
-		NRet: 0}, luar.New(s.L, raddr), luar.New(s.L, fp), luar.New(s.L, pi))
+	err := s.L.CallByParam(
+		lua.P{
+			Fn:   s.L.GetGlobal("onpathdown"),
+			NRet: 0,
+		},
+		lua.LString(raddr.String()),
+		lua.LString(fp),
+		new_lua_path_interface(pi),
+	)
 
 	log.Printf("OnPathDown called with fp %v and pi %v: %s", fp, pi, err)
 }
@@ -239,9 +217,10 @@ func (s *LuaSelector) Close(raddr pan.UDPAddr) error {
 
 	//call the "selectpath" function from the Lua script
 	//expect 1 return value
-	err := s.L.CallByParam(lua.P{
-		Fn:   s.L.GetGlobal("close"),
-		NRet: 1}, luar.New(s.L, raddr))
+	err := s.L.CallByParam(
+		lua.P{Fn: s.L.GetGlobal("close"),
+			NRet: 1},
+		lua.LString(raddr.String()))
 
 	log.Println("Close called on LuaSelector:", err)
 	//s.L.Close()
