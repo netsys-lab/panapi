222ebba7ed7e8a686ccb16425dbd3fc2a9715711
Thorben Krüger <thorben.krueger@ovgu.de>
2021-12-02 12:25:08 +0100

    using composition for lua state

---
+++ lua/lua.go
@@ -0,0 +1,37 @@
+package lua
+
+import (
+	"log"
+	"os"
+	"sync"
+
+	lua "github.com/yuin/gopher-lua"
+)
+
+type State struct {
+	*lua.LState
+	sync.Mutex
+	*log.Logger
+}
+
+func NewState() *State {
+	L := lua.NewState()
+	l := log.Default()
+	l.SetFlags(log.Ltime)
+	l.SetPrefix("lua ")
+	return &State{L, sync.Mutex{}, l}
+}
+
+func (s *State) LoadScript(fname string) error {
+	file, err := os.Open(fname)
+	if err != nil {
+		return err
+	}
+	if fn, err := s.Load(file, fname); err != nil {
+		return err
+	} else {
+		s.Printf("loaded selector from file %s", fname)
+		s.Push(fn)
+		return s.PCall(0, lua.MultRet, nil)
+	}
+}
