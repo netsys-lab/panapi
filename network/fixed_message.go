fe11a2bf3f29a923ee7c10ad4d012a2d497ede4a
Clemens Beck <clemens.beck97@gmx.de>
2022-03-08 15:19:23 +0100

    bugfix: could not read/write from/to FixedMessage         for src/dst payloads of > 32 KiB

--- network/fixed_message.go
+++ network/fixed_message.go
@@ -24,40 +24,41 @@ type FixedMessage struct {
 	writeindex int
 }
 
-func NewFixedMessageString(s string) *FixedMessage {
-	b := []byte(s)
+func NewFixedMessageByte(b []byte) *FixedMessage {
 	return &FixedMessage{b, 0, len(b)}
 }
 
-func NewFixedMessage(size int) *FixedMessage {
+func NewFixedMessage(size int64) *FixedMessage {
 	return &FixedMessage{make([]byte, size), 0, 0}
 }
 
-func (m FixedMessage) String() string {
+func (m *FixedMessage) String() string {
 	return string(m.b)
 }
 
 //TODO, add proper locking and reusability of buffer
-func (m FixedMessage) Read(p []byte) (i int, err error) {
-	for i = m.readindex; i < len(m.b) && i < len(p); i++ {
-		p[i] = m.b[i]
+func (m *FixedMessage) Read(p []byte) (i int, err error) {
+	for i = 0; i < len(p) && i < len(m.b)-m.readindex; i++ {
+		p[i] = m.b[m.readindex+i]
 	}
-	m.readindex = i
-	if i == len(m.b) {
+	m.readindex += i
+	if m.readindex == len(m.b) {
 		err = io.EOF
 	}
 	return
 }
 
 // Write appends the contents of p to the message m, return error network.EOM when the capacity is met or exceeded
-func (m FixedMessage) Write(p []byte) (i int, err error) {
+func (m *FixedMessage) Write(p []byte) (i int, err error) {
 	if m.writeindex == len(m.b) {
-		errors.New("No space left in message")
+		return 0, errors.New("No space left in message")
 	}
-	for i = m.writeindex; i < len(m.b) && i < len(p); i++ {
-		m.b[i] = p[i]
+
+	for i = 0; i < len(p) && i < len(m.b)-m.writeindex; i++ {
+		m.b[m.writeindex+i] = p[i]
 	}
-	m.writeindex = i
+
+	m.writeindex += i
 	if m.writeindex == len(m.b) {
 		err = EOM
 	}
